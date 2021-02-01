f51687220e45c522ad1b0077b774105f7a7e1b9f
Juan Pablo <c137dts@gmail.com>
2021-02-01 14:25:43 -0500

    Terminando el menu de navegacion

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,15 @@
 const BURGER_MENU = document.querySelector('.menu__btn')
+const NAV = document.querySelector('.nav')
+const HEADER = document.querySelector('.header')
 
 BURGER_MENU.addEventListener('click', () => {
-    (BURGER_MENU.classList.contains('open'))
-        ? BURGER_MENU.classList.remove('open')
-        : BURGER_MENU.classList.add('open')
+    if (BURGER_MENU.classList.contains('open')){
+        BURGER_MENU.classList.remove('open')
+        NAV.style.display = 'none'
+        HEADER.style.position = 'initial'
+    } else {
+        BURGER_MENU.classList.add('open')
+        NAV.style.display = 'flex'
+        HEADER.style.position = 'fixed'
+    }
 })
